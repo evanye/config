c20b0b550ae69a266b757baf9d02264a279eae41
Evan Ye <applepie31415@gmail.com>
2014-08-31 23:13:25 -0700

    added zsh/spf13 config

--- init.sh
+++ init.sh
@@ -1,15 +1,10 @@
-rm ~/.bash_profile
-ln -s ~/config/bash/.bash_profile ~/.bash_profile
+rm -f ~/.zshrc
+ln -s ~/config/zsh/.zshrc ~/.zshrc
 
-rm ~/.gitconfig
+rm -f ~/.gitconfig
 ln -s ~/config/git/.gitconfig ~/.gitconfig
 
-rm ~/.git-completion.bash
-ln -s ~/config/git/.git-completion.bash ~/.git-completion.bash
-
-rm ~/.inputrc
-ln -s ~/config/.inputrc ~/.inputrc
-
-rm ~/.vimrc
-ln -s ~/config/vim/.vimrc ~/.vimrc
-
+rm -f ~/.vimrc.local
+ln -s ~/config/vim/.vimrc.local ~/.vimrc.local
+rm -f ~/.vimrc.bundles.local
+ln -s ~/config/vim/.vimrc.bundles.local ~/.vimrc.bundles.local
